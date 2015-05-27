cea1014a540093f852697c4357c1600cad156f33
Jonathan Fok kan <jfokkan@gmail.com>
2015-05-27 19:15:10 -0400

    Fixed quicksort

--- quicksort.go
+++ quicksort.go
@@ -5,11 +5,26 @@ import (
 )
 
 func main() {
+	a := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+	b := []int{1, 7, 6, 9, 5, 89, 88, 66, 54, 437, 8100}
+	fmt.Printf("%v\n", qsort(a))
+	fmt.Printf("%v\n", qsort(b))
 }
 
-func quicksort(a []int) []int {
+func qsort(a []int) []int {
+	if len(a) < 2 {
+		return a
+	}
+	lo, hi := 0, len(a)-1
 
-}
-
-func qsort(a []int, lo, hi int) []int {
+	for i := range a {
+		if a[i] < a[hi] {
+			a[i], a[lo] = a[lo], a[i]
+			lo++
+		}
+	}
+	a[hi], a[lo] = a[lo], a[hi]
+	qsort(a[:lo])
+	qsort(a[lo:])
+	return a
 }
